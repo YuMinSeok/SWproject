9d7eed2612844d4bf91de7c7fc6d88be7c2478d7
YuMinSeok <67250951+YuMinSeok@users.noreply.github.com>
2020-06-29 21:53:58 +0900

    issue #2
    b와 마지막줄 """이 없었음 
    
    sum 과 mul의 출력이 맘대로 안됨

--- tem.py
+++ tem.py
@@ -1,14 +1,14 @@
-html = b"""
+html = """
 <html>
     <body>
         <form action="">
-             input number =
-            a = <input type="number" name="a"> <br><br>
+            a = <input type="number" name="a"> <br>
             b = <input type="number" name="b">
             <input type="submit">
         </form>
-        sum = %(a+b)d <br><br>
-        mul = %(a*b)d
+        sum = a+b <br>
+        mul = a*b
     </body>
 </html>
+"""
 
